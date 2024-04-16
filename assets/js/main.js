1eb35eaa18927b7fb8506667fdf5e1f4cc352f55
Noah Miles <noahemiles@gmail.com>
2024-04-16 11:56:46 -0700

    basic working circle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
 // main.js
+
+var numberOfElement = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var current = 0;
+function renderAngle(data) {
+    var angles = [];
+    var totalElements = data.length;
+    var angleIncrement = 360 / totalElements; // Calculate angle increment dynamically
+    for (var i = 1; i <= totalElements; i++) {
+      angles.push(i * angleIncrement);
+    }
+    return angles;
+  }
+  
+
+function generateHtml() {
+  var html = '';
+  current = 0;
+  var angles = renderAngle(numberOfElement);
+  angles.forEach(function(item, index) {
+    html += `<div class="shapes" style="--deg:${item}deg;"></div>`;
+  });
+  document.querySelector('.circle').innerHTML = html;
+}
+document.getElementById('candle-circle')?.addEventListener('click', toggleCandle);
+document.getElementById('resetButton')?.addEventListener('click', resetElements);
+
+function toggleCandle() {
+    //remove one candle at a time
+    var shapes = document.querySelectorAll('.shapes');
+    if (shapes && current < numberOfElement.length) {
+        console.log(current);
+        console.log(shapes[current]);
+        shapes[current].classList.add('off');
+        current++;
+    }
+}
+function resetElements() {
+    generateHtml();
+}
+
+generateHtml();
